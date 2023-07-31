0eda3b3bd03bb36a0ee1ad06325825ffc88ed527
haroonhafeez <haroonhafeez961@gamil.com>
2023-07-31 08:05:36 -0700

    backend intigiration

--- component/Item.js
+++ component/Item.js
@@ -1,14 +1,17 @@
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
-const Item = () => {
+import { getDatabase, ref, child, get, set } from "firebase/database";
+const Item = ({route,navigation}) => {
+    const {name,disc,image,origen,price,type}=route.params
+    console.log("name:",disc)
     return (
         <View style={{ backgroundColor: '#7fbf53' }} >
-            <View style={styles.bgimg} ><Image style={styles.img} source={require('../assets/veg(1).png')} /></View>
+            <View style={styles.bgimg} ><Image style={styles.img} source={{uri:image}} /></View>
             <View style={styles.btmView} >
-                <Text style={[styles.text, { fontSize: 36, marginTop: 40, fontWeight: '800' }]} >Boston Lettuce</Text>
-                <Text style={[styles.text, { fontSize: 20, fontWeight: 'bold' }]}>20 <Text style={{ fontSize: 14, color: '#9586A8', fontWeight: '400' }} >Rs /piece</Text></Text>
+                <Text style={[styles.text, { fontSize: 36, marginTop: 40, fontWeight: '800' }]} >{name}</Text>
+                <Text style={[styles.text, { fontSize: 20, fontWeight: 'bold' }]}>{price} <Text style={{ fontSize: 14, color: '#9586A8', fontWeight: '400' }} >{type}</Text></Text>
                 <Text style={{ fontSize: 12, color: '#0BCE83', fontWeight: '400' }} >~ 0.01$ /piece </Text>
-                <Text style={[styles.text, { fontWeight: '900', fontSize: 17, marginTop: 15 }]} >Spain</Text>
-                <Text style={[styles.text, { color: '#9586A8', fontSize: 15,lineHeight:25 }]}>Lettuce is an annual plant of the daisy family, Asteraceae. It is most often grown as a leaf vegetable, but sometimes for its stem and seeds. Lettuce is most often used for salads, although it is also seen in other kinds of food, such as soups, sandwiches and wraps; it can also be grilled.</Text>
+                <Text style={[styles.text, { fontWeight: '900', fontSize: 17, marginTop: 15 }]} >{origen}</Text>
+                <Text style={[styles.text, { color: '#9586A8', fontSize: 15,lineHeight:25 }]}>{disc}</Text>
                 <View style={{position:'absolute',bottom:20,left:25, marginTop:80, height: 50, width: '100%', flexDirection: 'row', alignItems: 'center' }} >
                     <Pressable style={{ height: 45, width: '20%', borderColor: '#9586A8', borderWidth: 1, justifyContent: 'center', alignItems: 'center', borderRadius: 3 }} >
                         <Image style={{ height: 20, width: 20 }} source={require('../assets/Vector.png')} />
